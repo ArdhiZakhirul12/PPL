d5f5ac0a576134cc65be0f16c2b089ab857da8d7
ArdhiZakhirul12 <53462558+ArdhiZakhirul12@users.noreply.github.com>
2023-06-04 17:47:34 +0700

    profil

--- resources/views/investor/profil/index.blade.php
+++ resources/views/investor/profil/index.blade.php
@@ -11,51 +11,209 @@
   {{ session('success') }}
 </div>
 @endif
-
-        {{-- <a href="/a/akun/{{ $user->id }}/edit" class="btn btn-warning me-2 mb-3"><span data-feather="edit-2"> </span> Ubah Akun</a>   --}}
-        <div class="container d-flex">
+  
+<div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex">
             @foreach ($mitras as $mitra)
-            @if ($mitra->status == 'active')
-                
-            
-            <div class="card p-3 me-4">
-                <div class="d-flex ">
-                    <div class="image">
-                @if($mitra->image)
-                <img src="{{ asset('storage/' . $mitra->image) }}" class="rounded" width="150" height="150" >
-                @else
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"    alt="avatar"
-             width="150 ">
-                @endif
-                </div>
-                <div class="ms-2" style="width: 100px">                   
-                   <h4 class="mb-0 mt-0">{{ $mitra->nama_usaha }}</h4>
-                   <span>{{ $mitra->user->name }}</span>
-                   <div class="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
-                    <div class="d-flex flex-column">
-                        <span class="articles">Blog</span>
-                        <span class="number1">{{ count($blogs) }}</span>                    
+            @if ($mitra->status == 'active')                
+<div class="col-xl-6 col-md-12">
+    <div class="card user-card-full">
+        <div class="row m-l-0 m-r-0">
+            <div class="col-sm-4 bg-c-lite-green user-profile">
+                <div class="card-block text-center text-white">
+                    <div class="m-b-25">
+                        @if ($mitra->image)                  
+              <img src="{{ asset('storage/' . $mitra->image) }}" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px; height:150px">
+              @else
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                class="rounded-circle img-fluid" style="width: 150px;">
+                  
+              @endif                      
                     </div>
-                    {{-- <div class="d-flex flex-column">
-                        <span class="followers">Followers</span>
-                        <span class="number2">980</span>                  
-                    </div> --}}
-                    <div class="d-flex flex-column">
-                        <span class="rating">Proyek</span>
-                        <span class="number3">{{ count($proyeks) }}</span>                  
-                    </div>                   
-                   </div>
-                   <div class="button mt-2 d-flex flex-row align-items-center">
+                    <h6 class="f-w-600">{{ $mitra->nama_usaha }}</h6>
+                    <p>Agroindustri</p>
+                    <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="card-block">
+                    <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p class="m-b-10 f-w-600">Email</p>
+                        <h6 class="text-muted f-w-400">{{ $mitra->user->email }}</h6>
+                    </div>
+                    <div class="col-sm-6">
+                        <p class="m-b-10 f-w-600">Phone</p>
+                        <h6 class="text-muted f-w-400">{{ $mitra->no_hp }}</h6>
+                        </div>
+                    </div>
+                    <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Bio</h6>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            {{-- <p class="m-b-10 f-w-600"></p> --}}
+                            <h6 class="text-muted  f-w-400">{{ $mitra->bio }}</h6>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="/i/profil/{{ $mitra->id }}" type="button" class="btn btn-primary">Kunjungi</a>
+                        </div>
+                        
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif          
+@endforeach
+    </div>
+    </div>
+</div>
 
-                    <a href="/i/profil/{{ $mitra->id }}" class="btn btn-outline-success btn-sm w-100">Lihat</a>
-                    {{-- <button class="btn btn-sm btn-primary w-100 ml-2">Follow</button>                     --}}
-                   </div>
-                </div>                    
-                </div>                
-            </div>   
-            @endif          
-            @endforeach
-         </div>
+      
   </section>
 
+{{--  --}}
 @endsection()
+<style>
+    body {
+    background-color: #f9f9fa
+}
+
+.padding {
+    padding: 3rem !important
+}
+
+.user-card-full {
+    overflow: hidden;
+}
+
+.card {
+    border-radius: 5px;
+    -webkit-box-shadow: 0 1px 20px 0 rgba(69,90,100,0.08);
+    box-shadow: 0 1px 20px 0 rgba(69,90,100,0.08);
+    border: none;
+    margin-bottom: 30px;
+}
+
+.m-r-0 {
+    margin-right: 0px;
+}
+
+.m-l-0 {
+    margin-left: 0px;
+}
+
+.user-card-full .user-profile {
+    border-radius: 5px 0 0 5px;
+}
+
+.bg-c-lite-green {
+        background: -webkit-gradient(linear, left top, right top, from(#f29263), to(#ee5a6f));
+    background: linear-gradient(to right, #ee5a6f, #f29263);
+}
+
+.user-profile {
+    padding: 20px 0;
+}
+
+.card-block {
+    padding: 1.25rem;
+}
+
+.m-b-25 {
+    margin-bottom: 25px;
+}
+
+.img-radius {
+    border-radius: 5px;
+}
+
+
+ 
+h6 {
+    font-size: 14px;
+}
+
+.card .card-block p {
+    line-height: 25px;
+}
+
+@media only screen and (min-width: 1400px){
+p {
+    font-size: 14px;
+}
+}
+
+.card-block {
+    padding: 1.25rem;
+}
+
+.b-b-default {
+    border-bottom: 1px solid #e0e0e0;
+}
+
+.m-b-20 {
+    margin-bottom: 20px;
+}
+
+.p-b-5 {
+    padding-bottom: 5px !important;
+}
+
+.card .card-block p {
+    line-height: 25px;
+}
+
+.m-b-10 {
+    margin-bottom: 10px;
+}
+
+.text-muted {
+    color: #919aa3 !important;
+}
+
+.b-b-default {
+    border-bottom: 1px solid #e0e0e0;
+}
+
+.f-w-600 {
+    font-weight: 600;
+}
+
+.m-b-20 {
+    margin-bottom: 20px;
+}
+
+.m-t-40 {
+    margin-top: 20px;
+}
+
+.p-b-5 {
+    padding-bottom: 5px !important;
+}
+
+.m-b-10 {
+    margin-bottom: 10px;
+}
+
+.m-t-40 {
+    margin-top: 20px;
+}
+
+.user-card-full .social-link li {
+    display: inline-block;
+}
+
+.user-card-full .social-link li a {
+    font-size: 20px;
+    margin: 0 10px 0 0;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+
+
+</style>
